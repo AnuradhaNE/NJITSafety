b50bfa2bfeb7fb92591d72b8d3ff057b139ef790
AnuradhaNE <adn24@njit.edu>
2017-03-19 04:43:59 -0400

    Settings

--- app/src/main/java/com/njit/njitsafety/Safety.java
+++ app/src/main/java/com/njit/njitsafety/Safety.java
@@ -246,7 +246,7 @@ public class Safety extends AppCompatActivity implements
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_safety, menu);
+//        getMenuInflater().inflate(R.menu.menu_safety, menu);
         return true;
     }
 
@@ -255,14 +255,15 @@ public class Safety extends AppCompatActivity implements
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+   //     int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
-        return super.onOptionsItemSelected(item);
+//        return super.onOptionsItemSelected(item);
+        return true;
     }
 }
 
