ca19f8ccaa97cfa4de628ea089e10a3573978c52
AnuradhaNE <adn24@njit.edu>
2017-03-26 07:27:12 -0400

    Login+Signup+Add

--- app/src/main/java/com/njit/njitsafety/ChooseAddresses.java
+++ app/src/main/java/com/njit/njitsafety/ChooseAddresses.java
@@ -2,6 +2,12 @@ package com.njit.njitsafety;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 
 public class ChooseAddresses extends AppCompatActivity {
 
@@ -9,5 +15,21 @@ public class ChooseAddresses extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_addresses);
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.i("hi", "Place: " + place.getName());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i("hey", "An error occurred: " + status);
+            }
+        });
     }
 }
